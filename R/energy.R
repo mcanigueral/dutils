32adafd7b134137fd76e153125f4aa14d46101da
Marc Cañigueral <m.canigueral@resourcefully.nl>
2020-12-19 12:25:20 +0100

    Created energy R file

--- R/energy.R
+++ R/energy.R
@@ -1,3 +1,119 @@
+
+# Energy flows ------------------------------------------------------------
+
+#' Get energy balance time-series
+#'
+#' Input data frame must have columns `consumption`, `production`.
+#' Output data frame has extra columns `balance`, `local`, `imported`, `exported`.
+#' Column `balance` is positive when there is more production than consumption.
+#'
+#' @param df
+#'
+#' @return tibble or data.frame
+#' @export
+#'
+#' @importFrom dplyr %>% mutate
+#' @importFrom rlang .data
+#'
+get_energy_balance <- function(df) {
+  df %>%
+    mutate(
+      balance = .data$production - .data$consumption,
+      local = ifelse(.data$balance > 0, .data$consumption, .data$production),
+      imported = .data$consumption - .data$local,
+      exported = .data$production - .data$local,
+    )
+}
+
+
+
+# Indicators -------------------------------------------------------------
+
+#' Obtain a list of energy indicators given the energy flows
+#'
+#' @param df data.frame or tibble, with columns `datetime`, `consumption`, `production` and `kg_co2_kwh` (in this order)
+#' @param kg_co2_kwh factor of CO2 kg emissions per kWh of energy consumed from the distribution grid
+#'
+#' @return named list
+#' @export
+#'
+#' @importFrom dplyr %>% mutate summarise
+#' @importFrom rlang .data
+#'
+get_energy_kpis <- function(df, kg_co2_kwh = 0.5) {
+  if (!('kg_co2_kwh' %in% names(df))) {
+    df[['kg_co2_kwh']] <- kg_co2_kwh
+  }
+  # Energy = Power * resolution(h)
+  resolution <- as.numeric(df$datetime[2] - df$datetime[1], unit='hours')
+
+  df2 <- df %>%
+    get_energy_balance() %>%
+    mutate(
+      kg_co2 = .data$kg_co2_kwh*.data$imported*resolution,
+    )
+
+  peak_to_grid_dttm <- df2$datetime[which(df2$exported == max(df2$exported, na.rm = T))][1]
+  peak_from_grid_dttm <- df2$datetime[which(df2$imported == max(df2$imported, na.rm = T))][1]
+
+  df2 %>%
+    summarise(
+      total_consumption = sum(.data$consumption*resolution, na.rm = T),
+      total_production = sum(.data$production*resolution, na.rm = T),
+      total_local = sum(.data$local*resolution, na.rm = T),
+      total_exported = sum(.data$exported*resolution, na.rm = T),
+      total_imported = sum(.data$imported*resolution, na.rm = T),
+      selfconsumption = .data$total_local/.data$total_production,
+      selfsufficiency = .data$total_local/.data$total_consumption,
+      peak_to_grid = max(.data$exported, na.rm = T),
+      peak_from_grid = max(.data$imported, na.rm = T),
+      peak_to_grid_dttm = peak_to_grid_dttm,
+      peak_from_grid_dttm = peak_from_grid_dttm,
+      kg_co2 = sum(.data$kg_co2, na.rm = T)
+    ) %>%
+    as.list()
+}
+
+#' For each month independently, obtain a list of energy indicators given the energy flows
+#'
+#' @param df data.frame or tibble, with columns `datetime`, `consumption`, `production` and `kg_co2_kwh` (in this order)
+#'
+#' @return named list
+#' @export
+#'
+#' @importFrom dplyr %>% mutate group_by select everything
+#' @importFrom lubridate month
+#' @importFrom tidyr nest
+#' @importFrom purrr map_dfr
+#' @importFrom rlang .data
+#'
+get_monthly_energy_kpis <- function(df) {
+  d_nest <- df %>%
+    mutate(month = month(.data$datetime)) %>%
+    group_by(.data$month) %>%
+    nest()
+  map_dfr(d_nest$data, ~ get_energy_kpis(.x)) %>%
+    mutate(month = d_nest$month) %>%
+    select(.data$month, everything())
+}
+
+
+#' Compare KPIs (final kpis - original kpis)
+#'
+#' @param kpis_o named list with original kpis values
+#' @param kpis_f named list with final kpis values
+#'
+#' @return named list
+#' @export
+#'
+#' @importFrom purrr possibly map2
+compare_kpis <- function(kpis_o, kpis_f) {
+  possible_subtraction <- possibly(`-`, otherwise = NULL)
+  map2(kpis_f, kpis_o, ~ possible_subtraction(.x, .y))
+}
+
+
+
 # Solar from PVGIS --------------------------------------------------------
 
 #' Get TMY from PVGIS given the latitude and longitude coordinates.
@@ -116,4 +232,3 @@ get_multiple_pv_timeseries <- function(year=2021, lat=52.370, lon=4.908, databas
     mutate(datetime = get_datetime_seq(year, "UTC", 60, fullyear=T)) %>%
     select(.data$datetime, everything())
 }
-
