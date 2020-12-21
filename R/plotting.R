0c98666dd2670de26504e3b64c8bb0eabf336077
Marc Cañigueral <m.canigueral@resourcefully.nl>
2020-12-21 10:08:31 +0100

    Introduced custom dygraphs formatting CSS file.

--- R/plotting.R
+++ R/plotting.R
@@ -20,7 +20,7 @@ df_to_ts <- function(df) {
 #' Format Dygraphs plot with CSS file and extra options
 #'
 #' @param dyplot dygraph object
-#' @param css_file character path to a CSS file to format dygraph plot. If NULL, no CSS formating is applied to dygraph.
+#' @param css_file character path to a CSS file to format dygraph plot. If NULL, custom CSS is applied to dygraph.
 #' @param ... extra arguments to pass to `dygraphs::dyOptions` function
 #'
 #' @return dygraphs plot
@@ -30,19 +30,18 @@ df_to_ts <- function(df) {
 #' @importFrom dygraphs dyLegend dyOptions dyCSS
 #'
 #' @details
-#' Atention! By default this function needs a CSS file in WWW folder.
+#' Atention! By default this function applies custom CSS file.
 #'
-format_dygraph <- function(dyplot, css_file="www/style.css", ...) {
+format_dygraph <- function(dyplot, css_file=NULL, ...) {
   dyplot <- dyplot %>%
     dyLegend(show = "onmouseover") %>%
     dyOptions(retainDateWindow = TRUE,
               useDataTimezone = TRUE,
               ...)
   if (is.null(css_file)) {
-    return( dyplot )
-  } else {
-    dyplot %>% dyCSS(css_file)
+    css_file <- system.file("www", "dystyle.css", package = "dutils")
   }
+  dyplot %>% dyCSS(css_file)
 }
 
 
@@ -52,7 +51,7 @@ format_dygraph <- function(dyplot, css_file="www/style.css", ...) {
 #' @param df data.frame or tibble, first column of name `datetime` being of class datetime and rest of columns being numeric
 #' @param plot_conf tibble of the plot configuration with columns `variable`, `label`, `color`, `fill` and `width` to pass to dygraph.
 #' @param ylab character, label of y axis
-#' @param css_file character path to a CSS file to format dygraph plot. If NULL, no CSS formating is applied to dygraph.
+#' @param css_file character path to a CSS file to format dygraph plot. If NULL, custom CSS is applied to dygraph.
 #' @param ... extra arguments to pass to `dygraphs::dyOptions` function
 #'
 #' @return dygraphs plot
@@ -63,9 +62,9 @@ format_dygraph <- function(dyplot, css_file="www/style.css", ...) {
 #' @importFrom purrr transpose
 #'
 #' @details
-#' Atention! By default this function needs a CSS file in WWW folder.
+#' Atention! By default this function applies custom CSS file.
 #'
-plot_components <- function(df, plot_conf, ylab = "kW", css_file = "www/style.css", ...) {
+plot_components <- function(df, plot_conf, ylab = "kW", css_file = NULL, ...) {
   dyplot <- df %>% df_to_ts() %>% dygraph(group = "a", ylab = ylab)
   for (component in transpose(plot_conf)) {
     if (component$variable %in% names(df)) {
