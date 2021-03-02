32438396caa7d685b153fcc5e38e55ebea540531
Marc Cañigueral <m.canigueral@resourcefully.nl>
2021-03-02 08:39:25 +0100

    Introduced DynamoDB support

---
+++ R/dynamodb.R
@@ -0,0 +1,168 @@
+
+#' Get data from a specific partition key
+#'
+#' @param dynamodb boto3 DynamoDB object
+#' @param table_name character, table name
+#' @param id character, partition key ID
+#' @param start_date Date, start date
+#' @param end_date Date, end date
+#' @param tzone character, time zone
+#'
+#' @return tibble
+#' @export
+#'
+#' @importFrom purrr pmap_dfr
+#' @importFrom dplyr %>% select
+#' @importFrom rlang .data
+get_id_table <- function(dynamodb, table_name, id, start_date, end_date, tzone = "Europe/Paris") {
+  dynamodb_table <- get_dynamodb_table(table_name)
+  if (is.null(dynamodb_table)) return( NULL )
+
+  time_range <- adapt_date_range(start_date, end_date)
+
+  pmap_dfr(
+    time_range,
+    ~ query_table(dynamodb_table, id, ..1, ..2)
+  )  %>%
+    select(.data$id, .data$timestamp, .data$data)  %>%
+    adapt_table_format(tzone)
+}
+
+
+#' Get boto3 DynamoDB Table object
+#'
+#' @param dynamodb boto3 DynamoDB object
+#' @param table_name character, table name
+#'
+#' @return boto3 DynamoDB Table object
+#' @export
+#'
+get_dynamodb_table <- function(dynamodb, table_name) {
+  dynamodb_table <- dynamodb$Table(table_name)
+  items_count <- count_table_items(dynamodb_table)
+  if (!is.null(items_count) & (items_count > 0)) {
+    return( dynamodb_table )
+  } else {
+    if (!is.null(items_count)) message("This table does not exist.")
+    if (items_count <= 0) message("This table is empty.")
+    return( NULL )
+  }
+}
+
+table_item_count <- function(table) {
+  table$item_count
+}
+count_table_items <- purrr::possibly(table_item_count, otherwise = NULL)
+
+
+#' Convert start and end dates to DynamoDB timestamp
+#'
+#' @param start_date Date, start date
+#' @param end_date Date, end date
+#'
+#' @return tibble
+#' @export
+#'
+#' @importFrom dplyr tibble select %>%
+#' @importFrom rlang .data
+#' @importFrom lubridate days
+#'
+adapt_date_range <- function(start_date, end_date) {
+  if (as.integer(end_date - start_date, units = 'days') > 30) {
+    tibble(
+      start.date = seq.POSIXt(start_date, end_date, by = '30 days'),
+      end.date = start_date + days(30),
+      start.timestamp = as.integer(.data$start.date)*1000,
+      end.timestamp = as.integer(.data$end.date)*1000
+    ) %>%
+      select(.data$start.timestamp, .data$end.timestamp)
+  } else {
+    tibble(
+      start.timestamp = as.integer(start_date)*1000,
+      end.timestamp = as.integer(end_date)*1000
+    )
+  }
+}
+
+
+#' From non-SQL DynamoDB table to spread tibble
+#'
+#' @param table boto3 DynamoDB table object
+#' @param tzone character, time zone
+#'
+#' @return tibble
+#' @export
+#'
+#' @importFrom dplyr as_tibble mutate select %>%
+#' @importFrom lubridate as_datetime
+#' @importFrom xts align.time
+#' @importFrom rlang .data
+#'
+adapt_table_format <- function(table, tzone = "Europe/Paris") {
+  table %>%
+    as_tibble() %>%
+    mutate(
+      datetime = as_datetime(.data$timestamp/1000, tz = tzone),
+      datetime = align.time(.data$datetime, n=60*5)
+    ) %>%
+    select(.data$id, .data$datetime, .data$data) %>%
+    spread_data_column()
+}
+
+spread_data_column <- function(table) {
+  dplyr::select(
+    dplyr::mutate(
+      table,
+      purrr::map_dfr(
+        rlang::.data$data,
+        ~ spread_data_item(.x)
+      )
+    ),
+    -'data'
+  )
+}
+
+spread_data_item <- function(item) {
+  tibble::as_tibble_row(purrr::map(item, ~ parse_python_object(.x)))
+}
+
+
+#' Python to R parsing for DynamoDB items
+#'
+#' @param object Python object of class decimal.Decimal, character or logical. For other types a NA value will be returned.
+#'
+#' @return R object
+#' @export
+#'
+parse_python_object <- function(object) {
+  if ('decimal.Decimal' %in% class(object)) {
+    return( as.numeric(as.character(object)) )
+  } else if ('character' == class(object)) {
+    return( as.character(object) )
+  } else if ('logical' == class(object)) {
+    return( as.logical(object) )
+  } else {
+    return( NA )
+  }
+}
+
+
+# Wrap python functions ---------------------------------------------------
+
+query_table <- function(dynamodb_table, user_id, start_timestamp, end_timestamp) {
+  # reticulate::source_python(system.file("python/utils.py", package = 'evflex'), envir = pyenv)
+
+  python_path <- system.file("python", package = "dutils")
+  tools <- reticulate::import_from_path("dynamodb", path = python_path)
+  tools$utils$query_table(dynamodb_table, user_id, start_timestamp, end_timestamp)
+}
+
+
+
+
+
+
+
+
+
+
---
+++ inst/python/dynamodb/utils.py
@@ -0,0 +1,43 @@
+
+from decimal import Decimal
+from boto3.dynamodb.conditions import Key, Attr
+import pandas as pd
+
+
+def query_table(table, user_id, start, end):
+    items = table.query(KeyConditionExpression = Key("id").eq(user_id) & Key('timestamp').between(Decimal(start), Decimal(end)))['Items']
+    if (len(items) == 0): return None
+    items_df = pd.DataFrame(items)
+    if ('data' not in items_df): return None
+    items_df['id'] = [str(i) for i in items_df['id']]
+    items_df['timestamp'] = [int(i) for i in items_df['timestamp']]
+    if items_df['data'].isnull().values.any():
+        items_df = items_df.fillna(method='ffill')
+    items_df['data'] = [dict(i) for i in items_df['data']]
+    return items_df
+
+
+def put_df(table, df):
+    with table.batch_writer() as batch:
+        for i in range(df.shape[0]):
+            batch.put_item(
+                Item={
+                    'id': str(df['id'][i]),
+                    'timestamp': int(df['timestamp'][i]),
+                    'data': dict(df['data'][i])
+                }
+            )
+        
+        
+def delete_df(table, df):
+    with table.batch_writer() as batch:
+        for i in range(df.shape[0]):
+            batch.delete_item(
+                Key={
+                    'id': str(df['id'][i]),
+                    'timestamp': int(df['timestamp'][i])
+                }
+            )
+        
+        
+        
