7624517b27a5a83ffefa80e22bebd860f7fc2a25
Marc Cañigueral <marc.canigueral@udg.edu>
2023-11-27 21:57:53 +0100

    Improving `plot_components` function

--- R/plotting.R
+++ R/plotting.R
@@ -108,7 +108,10 @@ plot_components <- function(df, plot_conf, ylab, legend_width = NULL, css_file =
       if (is.null(legend_width)) {
         # The legend width is calculated using a relation of 9 pixels = 1 character
         # also including the line symbol and margins applied to dyLegend box (+14)
-        min_legend_width <- max(min_legend_width, 9*stringr::str_length(component$label)+14)
+        min_legend_width <- max(
+          min_legend_width,
+          9*stringr::str_length(paste(component$label, max(df[[component]])))+14
+        )
       }
     }
   }
